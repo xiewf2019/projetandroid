ed5aad8e6f8cdfb85fbaa0c364dfd619f7dee1ab
xiewf2019 <50437346+xiewf2019@users.noreply.github.com>
2020-05-22 16:05:31 +0200

    petite échange

--- show_district.php
+++ show_district.php
@@ -3,7 +3,7 @@ $list1 = $_REQUEST['list1'];
 $list2 = $_REQUEST['list2'];
 $list3 = $_REQUEST['list3'];
 $d='python3 extrapole.py ';
-$command1='python extrapole.py '.$list1." ".$list2." ".$list3." 2>&1";
+$command1='python extrapole.py '.$list1." ".$list2." ".$list3." ";
 
 $command  = escapeshellcmd('python extrapole.py '.$list1." ".$list2." ".$list3);
 $output = shell_exec($command);
